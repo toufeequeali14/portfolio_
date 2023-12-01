3313508512be14a08bc14fea6ecc005c7b22bc78
Toufeeque Ali <130752892+toufeequeali14@users.noreply.github.com>
2023-12-01 16:56:59 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection"; 
+import Footer from "./components/Footer"; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <HeroSection />
+    <Footer />
+    <middle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
